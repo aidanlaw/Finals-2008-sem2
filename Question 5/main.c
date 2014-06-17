c20ac5ac252fe3b901d20c72fac6ff2c5386b022
aidanlaw <law.aidan@gmail.com>
2014-06-17 21:27:27 +0800

    2127 17th June

---
+++ Question 5/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  Question 5
+//
+//  Created by Aidan Law on 17/06/2014.
+//  Copyright (c) 2014 Aidan Law. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    struct image                        //this is similar to setting up an integer, (*type of variable* *name of variable*)
+    {                                   //anything within the curly-wurlys are part of the struct being formed
+        int width;                      //creates an integer value called width
+        int height;                     //creates an integer value called height
+        unsigned char image[480[640]];  //creates an array 480 rows and 640 columns within the struct called image
+    };                                  //ends the section 
+    
+    
+    
+    return 0;
+}
+
